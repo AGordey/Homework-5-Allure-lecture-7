2a1e6c1fafc89885e2ac6f948492c8992dd292cc
AGordey <gordey.andrej@gmail.com>
2022-04-17 22:36:01 +0300

    3. Шаги с аннотацией @Step

---
+++ src/test/java/qa/guru/TestWithAnnotationSteps.java
@@ -0,0 +1,47 @@
+package qa.guru;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class TestWithAnnotationSteps {
+
+    private static final String REPOSITORY = "AGordey/Homework-7-Allure";
+    private static final int ISSUESNUMBER = 1;
+
+    @Test
+    public void testGithubIssueFailed() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Переход на Github", () -> {
+            open("https://github.com");
+        });
+        step("Поиск репозитория с ISSUE " + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys("AGordey/Homework-7-Allure");
+            $(".header-search-input").submit();
+        });
+        step("Преходим из выдачи поиска искомому репозиторию: " + REPOSITORY, () -> {
+            $(linkText("AGordey/Homework-7-Allure")).click();
+        });
+        step("Переходим во вкладку ISSUES", () -> {
+            $(partialLinkText("Issues100")).click(); // добавил цифру 100 для ошибки теста
+        });
+        step("Поиск ISSUES с номером " + ISSUESNUMBER, () -> {
+            $(withText("1")).click();
+        });
+    }
+}
+
+
+
+
+
+
+
