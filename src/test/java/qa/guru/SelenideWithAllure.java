b4a1344e933110bf85aa39242cd4d62bc2b22e9d
AGordey <gordey.andrej@gmail.com>
2022-04-17 20:52:58 +0300

    Base test commit

--- src/test/java/qa/guru/SelenideWithAllure.java
+++ src/test/java/qa/guru/SelenideWithAllure.java
@@ -1,16 +1,38 @@
 package qa.guru;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
 
 public class SelenideWithAllure {
 
+
     @Test
-    public void
+    public void testGithubIssue () {
 
+        open("https://github.com");
 
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("AGordey/Homework-7-Allure");
+        $(".header-search-input").submit();
 
-
-
-
+        $(linkText("AGordey/Homework-7-Allure")).click();
+        $(partialLinkText("Issues")).click();
+        $(withText("#1")).click();
+    }
 
 }
+
+
+
+
+
+
+
